b7bebf2133cc36d07915336dfa4861c1376b4d98
krmkrmsmt-netizen <krmkrmsmt@gmail.com>
2026-08-23 12:14:18 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>QR Menü</h1>
+      <p>Kafeler için dijital menü sistemi</p>
+
+      <button>Ücretsiz Menü Oluştur</button>
+      <button>Giriş Yap</button>
+    </div>
+  );
+}
+
+export default App;
